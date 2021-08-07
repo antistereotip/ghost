aa63ab4047bddd64bc8214ca98c134d5b1d02240
antistereotip <milutingavrilovic@yahoo.com>
2021-08-07 20:39:21 +0200

    pointers.c

---
+++ ZERO-SQUAD/pointers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+
+   int  hightech;
+   char kgb[10];
+
+   printf("Address of hightech variable: %x\n", &hightech  );
+   printf("Address of kgb variable: %x\n", &kgb  );
+
+   return 0;
+}
